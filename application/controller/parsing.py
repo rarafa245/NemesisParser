c5acecca5250280f6279854f0ca69cb47a857cf2
Rafael <rarafa245@gmail.com>
2020-08-29 01:29:01 -0300

    First Tests with parser

--- application/controller/parsing.py
+++ application/controller/parsing.py
@@ -5,9 +5,20 @@ def parsing_message(message: bool) -> Dict:
     str_message = str(message).split("'")[1]
 
     message_header = str_message[:4]
-    message_type = str_message[4:10]
-    message_payload = str_message[10:-4]
+    message_device = str_message[4:10]
+    message_type = str_message[10:12]
+    message_payload = str_message[12:-4]
     message_footer = str_message[-4:]
+
+    parsed_message = {
+        "HEADER":   message_header,
+        "DEVICE":   message_device,
+        "TYPE":     message_type,
+        "PAYLOAD":  message_payload,
+        "FOOTER":   message_footer
+    }
+
+    return parsed_message
 
 
 def parsering_message_payload(payload: str):
