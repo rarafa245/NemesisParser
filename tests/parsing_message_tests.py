2f956081f33b00ee9b1041af1f6bc4eccb1d57c6
Rafael <rarafa245@gmail.com>
2020-08-29 21:30:56 -0300

    Refatoring Tests to parsing payload

--- tests/parsing_message_tests.py
+++ tests/parsing_message_tests.py
@@ -3,26 +3,47 @@ import pytest
 
 @pytest.mark.parametrize('message, result',
 [
+    # Testing normal message location
     (bytearray('50F70A3F73025EFCF950156F017D784000008CA0F8003C013026A1029E72BD73C4', "utf8"), 
         {
             "HEADER": b'50F7',
             "DEVICE": b'0A3F73',
             "TYPE": b'02',
-            "PAYLOAD": b'5EFCF950156F017D784000008CA0F8003C013026A1029E72BD',
+            "PAYLOAD": {
+                        "TIMESTAMP": b'5EFCF950',
+                        "DIRECTION": b'156F',
+                        "DISTANCE": b'017D7840',
+                        "TIME_ON": b'00008CA0',
+                        "COMPOSER": b'F800',
+                        "SPEED": b'3C',
+                        "LATITUDE": b'013026A1',
+                        "LONGITUDE": b'029E72BD'
+                        },
             "FOOTER": b'73C4'
         }
     ),
 
-    (bytearray('50F70A3F730273C4', "utf8"), 
+    # Testing normal message location without payload
+    (bytearray('50F70A3F730273C4', "utf8"),     
         {
             "HEADER": b'50F7',
             "DEVICE": b'0A3F73',
             "TYPE": b'02',
-            "PAYLOAD": b'',
+            "PAYLOAD": {
+                        "TIMESTAMP": b'',
+                        "DIRECTION": b'',
+                        "DISTANCE": b'',
+                        "TIME_ON": b'',
+                        "COMPOSER": b'',
+                        "SPEED": b'',
+                        "LATITUDE": b'',
+                        "LONGITUDE": b''
+                        },
             "FOOTER": b'73C4'
         }
     ),
 
+    # testing normal message ping
     (bytearray('50F70A3F730150494E4773C4', "utf8"), 
         {
             "HEADER": b'50F7',
@@ -33,6 +54,7 @@ import pytest
         }
     ),
 
+    # Testing message with header error
     (bytearray('55F70A3F73025EFCF950156F017D784000008CA0F8003C013026A1029E72BD73C4', "utf8"), 
         {
             "HEADER":   b'0000',
@@ -43,6 +65,7 @@ import pytest
         }
     ),
 
+    # Testing message with footer error
     (bytearray('50F70A3F73025EFCF950156F017D784000008CA0F8003C013026A1029E72BD77C4', "utf8"), 
         {
             "HEADER":   b'0000',
@@ -53,6 +76,7 @@ import pytest
         }
     ),
 
+    # Testing message with more caracteres
     (bytearray('150F70A3F73025EFCF950156F017D784000008CA0F8003C013026A1029E72BD73C4', "utf8"), 
         {
             "HEADER":   b'0000',
@@ -63,6 +87,7 @@ import pytest
         }
     ),
 
+    # Testing message with header and footer error
     (bytearray('50F70A3F73025EFCF950156F017D784000008CA0F8003C013026A1029E72BD73C41', "utf8"), 
         {
             "HEADER":   b'0000',
