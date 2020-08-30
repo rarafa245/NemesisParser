717a7fcdd37a561a2ea163c4c05a77c4332f5191
Rafael <rarafa245@gmail.com>
2020-08-30 02:06:42 -0300

    First Traducing Operation

--- application/controller/parser_handler.py
+++ application/controller/parser_handler.py
@@ -4,11 +4,16 @@ from .traducer_precesses import traducing_message
 
 def on_received_message(message: str):
 
-
+    # Parsing
     parsed_messsage = parsing_message(message)
+    print(parsed_messsage)
 
+    # Traducing
     if parsed_messsage["TYPE"] == b'02':
         traducing_message(parsed_messsage)
+
+    # Acting
+
 
     return parsed_messsage
     
--- application/controller/traducer_precesses/traducer.py
+++ application/controller/traducer_precesses/traducer.py
@@ -2,4 +2,26 @@ from typing import Dict
 
 
 def traducing_message(message: Dict) -> Dict:
-    print(message)
+    
+    # Getting relevant initial informations
+    message_type = 'loc'
+    payload = message['PAYLOAD']
+    
+    # Converting payload elements in int
+    raw_timestamp = int(payload["TIMESTAMP"], 16)     # form
+    angle = int(payload["DIRECTION"], 16)         
+    distance = int(payload["DISTANCE"], 16)       # form metros - km
+    time_on = int(payload["TIME_ON"], 16)         
+    speed = int(payload["SPEED"], 16)             
+    raw_latitude = int(payload["LATITUDE"], 16)       # form
+    raw_longitude = int(payload["LONGITUDE"], 16)     # form
+
+    composer = payload["COMPOSER"]
+
+
+def check_composer():
+    pass
+
+
+def check_bit(byte, position):
+    pass
